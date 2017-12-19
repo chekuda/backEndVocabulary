681685ade4a718d5b13b10cf238d9b720bb2bfbb
Jose Luis Checa Exposito <joselchecaexpo@gmail.com>
2017-12-19 18:36:25 +0000

    getListOftests

--- server-config/api/controllers/controllers.js
+++ server-config/api/controllers/controllers.js
@@ -1,7 +1,7 @@
 const commonPaths = require('../../commons-paths/common-paths')
 const fs = require('fs');
 
-const date = new Date().toISOString().split('T')[0]
+const date = new Date().getTime()
 
 exports.getvocabulary = (req, res) => {
   const file = req.params.file
@@ -11,10 +11,43 @@ exports.getvocabulary = (req, res) => {
 exports.savetest = (req, res) => {
   fs.writeFile(`${commonPaths.serviceFiles}/listTest/${date}.json`, JSON.stringify(req.body), (err) => {
     if (err){
-      res.send({ sucess: false })
+      res.send({ success: false })
     }
 
     console.log("The file was succesfully saved!");
-    res.send({ sucess: true })
+    res.send({ success: true })
+  });
+}
+
+exports.getListOfTests = (req, res) => {
+  const path = `${commonPaths.serviceFiles}/listTest`
+
+  const getFileNumber = (file) => {
+    return (file.match(/[0-9]+/g) || [])[0]
+  }
+
+  const orderList = (list) => {
+    return list
+            .reduce((acc, ele) => getFileNumber(ele) ? acc.concat(getFileNumber(ele)) : acc, [])
+            .sort((a, b) => b - a)
+  }
+
+  const createListObject = (list) => {
+    const listOrdered = orderList(list)
+
+      return listOrdered.map(element => {
+        return {
+          file: element,
+          data: require(`${path}/${element}.json`)
+        }
+      })
+  }
+
+  fs.readdir(path, (err, items) => {
+    if(err) {
+      res.send({ success: false })
+    }
+    orderList(items)
+   res.send({ success: true, listOfTest: createListObject(items) })
   });
 }
--- server-config/api/routes/routes.js
+++ server-config/api/routes/routes.js
@@ -5,6 +5,9 @@ module.exports = (app) => {
   app.route('/api/getvocabulary/:file')
     .get(listControllers.getvocabulary)
 
+  app.route('/api/getListOfTests')
+    .get(listControllers.getListOfTests)
+
   app.route('/api/savetest')
     .post(listControllers.savetest)
 }
