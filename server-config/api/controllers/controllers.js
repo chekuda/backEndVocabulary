7dec83d9868032182db6a3431b0beb03d4ef4280
Jose Luis Checa Exposito <joselchecaexpo@gmail.com>
2017-12-19 10:14:40 +0000

    add savetest

--- server-config/api/controllers/controllers.js
+++ server-config/api/controllers/controllers.js
@@ -1,6 +1,20 @@
 const commonPaths = require('../../commons-paths/common-paths')
+const fs = require('fs');
+
+const date = new Date().toISOString().split('T')[0]
 
 exports.getvocabulary = (req, res) => {
   const file = req.params.file
   res.sendFile(`${commonPaths.serviceFiles}/${file}.json`)
+}
+
+exports.savetest = (req, res) => {
+  fs.writeFile(`${commonPaths.serviceFiles}/listTest/${date}.json`, JSON.stringify(req.body), (err) => {
+    if (err){
+      res.send({ sucess: false })
+    }
+
+    console.log("The file was succesfully saved!");
+    res.send({ sucess: true })
+  });
 }
--- server-config/api/routes/routes.js
+++ server-config/api/routes/routes.js
@@ -4,4 +4,7 @@ const listControllers = require('../controllers/controllers')
 module.exports = (app) => {
   app.route('/api/getvocabulary/:file')
     .get(listControllers.getvocabulary)
+
+  app.route('/api/savetest')
+    .post(listControllers.savetest)
 }
